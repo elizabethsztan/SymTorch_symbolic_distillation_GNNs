c3533a81f6a829d5595f9aa2045c08c5f26209ee
Liz Tan <elizabethsztan@gmail.com>
2025-08-09 08:22:17 +0800

    train the new Pruning_MLP models

--- train_models_r1.py
+++ train_models_r1.py
@@ -9,7 +9,7 @@ import argparse
 #split the data into train and test sets
 
 """
-This script trains all the models on the charge dataset. It is connected to wandb.
+This script trains all the models on the r1 dataset. It is connected to wandb.
 To run 
 $ train_models_r1.py --save --wandb_log --epoch 100
 """
@@ -27,34 +27,34 @@ def main():
 
     train_data, val_data, _ = load_and_process(data_path, seed)
 
-    print("\n=== Testing standard Model ===")
-    model_type = 'standard'
-    model = create_model(model_type = model_type)
-    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
-                  save=args.save, wandb_log=args.wandb_log)
+    # print("\n=== Testing standard Model ===")
+    # model_type = 'standard'
+    # model = create_model(model_type = model_type)
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
+    #               save=args.save, wandb_log=args.wandb_log)
 
-    print("\n=== Testing L1 Model ===")
-    model_type = 'L1'
-    model = create_model(model_type = model_type)
-    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
-                  save=args.save, wandb_log=args.wandb_log)
+    # print("\n=== Testing L1 Model ===")
+    # model_type = 'L1'
+    # model = create_model(model_type = model_type)
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
+    #               save=args.save, wandb_log=args.wandb_log)
 
-    print("\n=== Testing bottleneck Model ===")
-    model_type = 'bottleneck'
-    model = create_model(model_type = model_type)
-    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
-                  save=args.save, wandb_log=args.wandb_log)
+    # print("\n=== Testing bottleneck Model ===")
+    # model_type = 'bottleneck'
+    # model = create_model(model_type = model_type)
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
+    #               save=args.save, wandb_log=args.wandb_log)
 
-    print("\n=== Testing KL Model ===")
-    model_type = 'KL'
-    model = create_model(model_type = model_type)
-    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
-                  save=args.save, wandb_log=args.wandb_log)
+    # print("\n=== Testing KL Model ===")
+    # model_type = 'KL'
+    # model = create_model(model_type = model_type)
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
+    #               save=args.save, wandb_log=args.wandb_log)
 
     print("\n=== Testing pruning Model ===")
     model_type = 'pruning'
     model = create_model(model_type = model_type)
-    model.set_pruning_schedule(args.epoch, schedule='cosine', end_epoch_frac=0.65)
+    model.edge_model.set_schedule(args.epoch, decay_rate='cosine', end_epoch_frac=0.65)
     model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'r1', num_epoch=args.epoch,
                   save=args.save, wandb_log=args.wandb_log)
     
